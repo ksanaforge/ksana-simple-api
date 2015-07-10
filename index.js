efdd2f90b8794881355f307162eaca1424a0ca9f
yapcheahshen <yapcheahshen@gmail.com>
2015-07-10 20:20:57 +0800

    add filter field with regex

--- index.js
+++ index.js
@@ -2,16 +2,28 @@ var kse=require("ksana-search");
 var kde=require("ksana-database");
 var bsearch=kde.bsearch;
 
-
 var next=function(opts,cb,context) {
+	kse.search(opts.db,opts.q,function(err,res){
+		var db=res.engine;
+		if (err) {
+			cb(err);
+			return;
+		}
+		var out=[];
+		var next=opts.uti;
+		var count=opts.count||10;
+		for (var i=0;i<count;i++) {
+			var next=db.nextTxtid(next);
+			if (!next) break;
+			out.push(next);
+		}
+		opts.uti=out;
+		fetch(opts,cb,context);
+	});
 }
 var prev=function(opts,cb,context) {
 }
 
-var filesegwithhits=function(res) {
-	console.log(res)
-	//{t:texts[i],d:depths[i], vpos:voffs[i]}
-}
 var toc=function(opts,cb,context) {
 	var that=this;
 	kse.search(opts.db,opts.q,{},function(err,res){
@@ -38,12 +50,12 @@ var txtids2key=function(txtids) {
 	return out;
 }
 
-var hits2markup=function( Q,file,seg){
+var hits2markup=function( Q,file,seg, text){
 	var seg1=this.fileSegToAbsSeg(file,seg);
 	var vpos=this.absSegToVpos(seg1-1);
 	var vpos2=this.absSegToVpos(seg1);
 	//console.log("rawresult",Q.rawresult,vpos,vpos2)
-	var hits=kse.excerpt.hitInRange(Q,vpos,vpos2);
+	var hits=kse.excerpt.realHitInRange(Q,vpos,vpos2,text);
 	for (var i=0;i<hits.length;i++) {
 		hits[i][2]="HL"+hits[i][2];
 	}
@@ -51,6 +63,10 @@ var hits2markup=function( Q,file,seg){
 }
 var fetch=function(opts,cb,context) {
 	var that=this;
+	if (!opts.uti) {
+		cb("missing uti");
+		return;
+	}
 	kse.search(opts.db,opts.q,{},function(err,res){
 		if (err) {
 			cb(err);
@@ -59,7 +75,7 @@ var fetch=function(opts,cb,context) {
 			res.engine.get(keys,function(data){
 				var out=[];
 				for (var i=0;i<opts.uti.length;i++) {
-					var hits=hits2markup.call(res.engine,res,keys[i][1],keys[i][2]);
+					var hits=hits2markup.call(res.engine,res,keys[i][1],keys[i][2],data[i]);
 					out.push({uti:opts.uti[i],text:data[i],hits:hits});
 				}
 				cb(0,out);
@@ -67,7 +83,33 @@ var fetch=function(opts,cb,context) {
 		}
 	});	
 }
+
+var excerpt2defaultoutput=function(excerpt) {
+	var out=[];
+	for (var i=0;i<excerpt.length;i++) {
+		var ex=excerpt[i];
+		var vpos=this.fileSegToVpos(ex.file,ex.seg);
+		var txtid=this.vpos2txtid(vpos);
+		out.push({uti:txtid,text:ex.text,hits:ex.realHits});
+	}
+	return out;
+}
+//use startfrom to specifiy starting posting
 var excerpt=function(opts,cb,context) {
+	var that=this;
+	if (!opts.q) {
+		cb("missing q");
+		return;
+	}
+	kse.search(opts.db,opts.q,{nohighlight:true,range:{from:opts.from}},
+		function(err,res){
+		if (err) {
+			cb(err);
+		} else {
+			//console.log(res.rawresult)
+			cb(0,excerpt2defaultoutput.call(res.engine,res.excerpt));
+		}
+	});	
 }
 
 var beginWith=function(s,txtids) {
@@ -95,7 +137,39 @@ var scan=function(opts,cb,context) {
 		cb(0,out);
 	});
 }
+
+var filterField=function(items,regex) {
+	var reg=new RegExp(regex);
+	var out=[];
+	for (var i=0;i<items.length;i++) {
+		var item=items[i];
+		if (item.match(reg)) {
+			out.push(item);
+		}
+	}
+	return out;
+}
+
 var filter=function(opts,cb,context) {
+	kse.search(opts.db,opts.q,{},function(err,res){
+		if (err) {
+			cb(err);
+			return;
+		}
+		var db=res.engine;
+		var out=[];
+
+		if (opts.field) {
+			values=db.get(["fields",opts.field],function(data){
+				var matches=filterField(data,opts.regex);
+				cb(0,matches);
+			});
+		} else {//use uti
+			var values=db.get("txtid");
+			var matches=filterField(values,opts.regex);
+			cb(0,matches);
+		}
+	});
 }
 
 
--- test/test.js
+++ test/test.js
@@ -15,15 +15,52 @@ describe("api test",function(){
 	it("toc test",function(done){
 		ksa.toc({db:"sampledict",q:"內"},function(err,data){
 			//assert.deepEqual(data,["菩提","菩提心","學佛"]);
-			console.log(data)
+			//console.log(data)
+			assert(data.toc.length,9); // engine.segcount +2
 			done();
 		});
 	});
 
+  //fetch a given set of uti
 	it("fetch test",function(done){
 		ksa.fetch({db:"sampledict",q:"內",uti:["菩提","菩提心"]},function(err,data){
-			console.log(JSON.stringify(data))
+			//console.log(JSON.stringify(data))
+			assert(data.length,2);
 			done();
 		});
-	})
+	});
+
+  //next is based on fetch, given a uti, fetch next batch of text.
+  it("next uti",function(done){
+  	var next=ksa.next({db:"sampledict",q:"內",uti:"b",count:2},function(err,data){
+  		//console.log(JSON.stringify(data));
+  		assert(data.length,2);
+  		done();
+  	});
+  })
+
+	//fetch only text with hits
+	it("excerpt test",function(done){
+		ksa.excerpt({db:"sampledict",q:"內"},function(err,data){
+			//console.log(JSON.stringify(data));
+			assert(data.length,2);
+			done();
+		});
+	});
+
+	it("excerpt start from test",function(done){
+		ksa.excerpt({db:"sampledict",q:"菩提",from:"菩提"},function(err,data){
+			//console.log(JSON.stringify(data));
+			assert(data.length,1);
+			done();
+		});
+	});	
+
+	it("filter",function(done){
+		ksa.filter({db:"sampledict",regex:"[a-z]"},function(err,data){
+			//console.log(data)
+			assert(data.length,4);
+			done();
+		});
+	});
 })
